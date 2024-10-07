2723e2da7912fd46b31789c7eeb83eff65bfb5eb
Samir Hassan <hsamir347@gmail.com>
2024-10-07 14:39:42 -0400

    retrieve all gift detail, project done

--- client/src/pages/GiftDetails.jsx
+++ client/src/pages/GiftDetails.jsx
@@ -1,34 +1,45 @@
+import React, { useState, useEffect } from "react";
+import { useParams } from "react-router-dom";
+import "./GiftDetails.css";
 
+const GiftDetails = ({ data }) => {
+  const { id } = useParams();
+  const [gift, setGift] = useState({
+    id: 0,
+    name: "",
+    pricepoint: "",
+    audience: "",
+    image: "",
+    description: "",
+    submittedby: "",
+    submittedon: "",
+  });
 
-import React, {useState, useEffect} from 'react';
-import './GiftDetails.css'
+  useEffect(() => {
+    const fetchGiftById = async () => {
+      const response = await fetch(`http://localhost:3001/gifts/${id}`);
+      const data = await response.json();
+      setGift(json);
+    };
+    fetchGiftById();
+  }, [data, id]);
 
-const GiftDetails = ({data}) => {
-
-    const [gift, setGift] = useState({id: 0, name: "", pricepoint: "", audience: "", image: "", description: "", submittedby: "", submittedon: ""})
-
-
-    useEffect(() => {
-
-    }, []);
-
-
-    return (
-        <div className="GiftDetails">
-            <main id="gift-content" class="gift-info">
-                <div class="image-container">
-                    <img id="image" src={gift.image} />
-                </div>
-                <div class="gift-details">
-                    <h2 id="name">{gift.name}</h2>
-                    <p id="submittedBy">{'Submitted By: ' + gift.submittedby}</p>
-                    <p id="pricePoint">{'Price: ' + gift.pricepoint}</p>
-                    <p id="audience">{'Great For: ' + gift.audience}</p>
-                    <p id="description">{gift.description}</p>
-                </div>
-            </main>
+  return (
+    <div className="GiftDetails">
+      <main id="gift-content" class="gift-info">
+        <div class="image-container">
+          <img id="image" src={gift.image} />
         </div>
-    )
-}
+        <div class="gift-details">
+          <h2 id="name">{gift.name}</h2>
+          <p id="submittedBy">{"Submitted By: " + gift.submittedby}</p>
+          <p id="pricePoint">{"Price: " + gift.pricepoint}</p>
+          <p id="audience">{"Great For: " + gift.audience}</p>
+          <p id="description">{gift.description}</p>
+        </div>
+      </main>
+    </div>
+  );
+};
 
-export default GiftDetails
+export default GiftDetails;
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -1,19 +1,10 @@
 import express from "express";
-import path from "path";
-import GiftsController from '../controllers/gifts.js'
-
-
-import { fileURLToPath } from "url";
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+import GiftsController from "../controllers/gifts.js";
 
 const router = express.Router();
 
-router.get('/', GiftsController.getGifts)
+router.get("/", GiftsController.getGifts);
 
-router.get("/:giftId", (req, res) => {
-  res.status(200).sendFile(path.resolve(__dirname, "../public/gift.html"));
-});
+router.get("/:giftId", GiftsController.getGiftById);
 
-export default router
+export default router;
