17c67a594fbc974392eee64f6c545eda5a64c90c
Samir Hassan <hsamir347@gmail.com>
2024-10-07 14:00:32 -0400

    replaced frontend code

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,21 +1,7 @@
-import { defineConfig } from "vite";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
+// https://vitejs.dev/config/
 export default defineConfig({
-  // Configuration for building the project
-  build: {
-    // Output directory for the build files
-    outDir: "../server/public", // Places build files in the public folder of the server
-    // Whether to empty the output directory before building
-    emptyOutDir: true, // Ensures the output directory is cleared before each build
-  },
-  // Configuration for the development server
-  server: {
-    // Proxy configuration to redirect API calls during development
-    proxy: {
-      // Redirect requests starting with '/gifts' to the backend server
-      "/gifts": {
-        target: "http://localhost:3001", // The backend server where API requests are forwarded
-      },
-    },
-  },
-});
+  plugins: [react()],
+})
