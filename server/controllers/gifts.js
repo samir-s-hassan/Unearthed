ea1fd6de93d0325b4227b54fb6397e17e9de50e2
Samir Hassan <hsamir347@gmail.com>
2024-10-11 12:16:14 -0400

    new controllers and routes for create,update,delete and added express.json

--- server/controllers/gifts.js
+++ server/controllers/gifts.js
@@ -17,7 +17,73 @@ const getGiftById = async (req, res) => {
       WHERE id=$1
     `;
     const giftId = req.params.giftId;
-    const results = await db.query(selectQuery, [giftId]);
+    const results = await pool.query(selectQuery, [giftId]);
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const createGift = async (req, res) => {
+  try {
+    const {
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+    } = req.body;
+    const results = await pool.query(
+      `
+      INSERT INTO gifts (name, pricepoint, audience, image, description, submittedby, submittedon)
+      VALUES($1, $2, $3, $4, $5, $6, $7)
+      RETURNING *`,
+      [name, pricepoint, audience, image, description, submittedby, submittedon]
+    );
+    res.status(201).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const updateGift = async (req, res) => {
+  try {
+    const id = parseInt(req.params.giftId);
+    const {
+      name,
+      pricepoint,
+      audience,
+      image,
+      description,
+      submittedby,
+      submittedon,
+    } = req.body;
+    const results = await pool.query(
+      `
+    UPDATE gifts SET name = $1, pricepoint = $2, audience = $3, image = $4, description = $5, submittedby = $6, submittedon= $7 WHERE id = $8`,
+      [
+        name,
+        pricepoint,
+        audience,
+        image,
+        description,
+        submittedby,
+        submittedon,
+        id,
+      ]
+    );
+    res.status(200).json(results.rows[0]);
+  } catch (error) {
+    res.status(409).json({ error: error.message });
+  }
+};
+
+const deleteGift = async (req, res) => {
+  try {
+    const id = parseInt(req.params.giftId);
+    const results = await pool.query("DELETE FROM gifts WHERE id = $1", [id]);
     res.status(200).json(results.rows[0]);
   } catch (error) {
     res.status(409).json({ error: error.message });
@@ -27,4 +93,7 @@ const getGiftById = async (req, res) => {
 export default {
   getGifts,
   getGiftById,
+  createGift,
+  updateGift,
+  deleteGift
 };
--- server/routes/gifts.js
+++ server/routes/gifts.js
@@ -3,8 +3,14 @@ import GiftsController from "../controllers/gifts.js";
 
 const router = express.Router();
 
+//get all gifts
 router.get("/", GiftsController.getGifts);
-
+//get the gift pertaining to the ID
 router.get("/:giftId", GiftsController.getGiftById);
+//create a gift
+router.post("/", GiftsController.createGift);
+//delete the gift pertaining to this ID
+router.delete("/:giftId", GiftsController.deleteGift);
+router.patch("/:giftId", GiftsController.updateGift);
 
 export default router;
